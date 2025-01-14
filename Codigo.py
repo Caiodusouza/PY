f1cc6297c5bbfc21f476c29f697f32fffac1ed80
Caio Eduardo De Souza <127906616+Caiodusouza@users.noreply.github.com>
2025-01-14 09:41:08 -0300

    Add files via upload

---
+++ Codigo.py
@@ -0,0 +1,10 @@
+Faturamento = 1200
+Custo = 750.50
+Lucro = Faturamento - Custo
+
+print("Faturamento foi de", Faturamento)
+print("O custo foi de", Custo)
+print("O lucro foi de", Lucro)
+
+Faturamento = "O faturamento foi de tanto"
+Email = "EmailQualquer@gmail.com"
